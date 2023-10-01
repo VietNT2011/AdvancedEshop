c26f3542f32429dbf2bac4635ff03de9ead3adb1
Nguyen Thanh Viet <ntv142018@gmail.com>
2023-10-01 23:20:22 +0700

    version 1

---
+++ Components/Navbar.cs
@@ -0,0 +1,20 @@
+﻿using AdvancedEshop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedEshop.Components
+{
+    public class Navbar:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Navbar(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Categories.ToList());
+        }
+    }
+}
