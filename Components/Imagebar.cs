8fb11a438cd49cbd13d2475e45435b66f77393ed
Nguyen Thanh Viet <ntv142018@gmail.com>
2023-10-02 22:40:52 +0700

    version 1.1

---
+++ Components/Imagebar.cs
@@ -0,0 +1,20 @@
+﻿using AdvancedEshop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedEshop.Components
+{
+    public class Imagebar : ViewComponent
+    {
+            private readonly ApplicationDbContext _context;
+
+            public Imagebar(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public IViewComponentResult Invoke()
+            {
+                return View(_context.Categories.ToList());
+            }
+    }
+}
---
+++ Components/Trandy.cs
@@ -0,0 +1,19 @@
+﻿using AdvancedEshop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace AdvancedEshop.Components
+{
+    public class Trandy : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public Trandy(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Products.Where(p=>p.IsTrandy==true).ToList());
+        }
+    }
+}
